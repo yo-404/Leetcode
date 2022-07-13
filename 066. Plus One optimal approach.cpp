be741b9468c0be55f752fed028c349b2dd5a4df2
yo-404 <100558220+yo-404@users.noreply.github.com>
2022-07-14 01:44:11 +0530

    plus one
    optimal approach O(n)

---
+++ 066. Plus One optimal approach.cpp
@@ -0,0 +1,19 @@
+//66. Plus One leetcode
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i]++;
+                break;
+            }
+            else
+                digits[i]=0;
+        }if(digits[0]==0){
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
