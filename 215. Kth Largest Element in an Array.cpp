48cd7f9dd7e526b8dde1c2662c833aafa9d50be5
yo-404 <yogeshy2801@gmail.com>
2023-08-14 23:18:32 +0530

    maxHEAP Kth largest element

---
+++ 215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,23 @@
+//using maxHeap /Priority Queue
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        
+        for(int num :nums){
+            maxHeap.offer(num);
+        }
+
+        for(int i=0;i<k-1;i++){
+            maxHeap.poll();
+        }
+
+        return maxHeap.peek();
+        
+    }
+}
+
+
+// pushing all the values in maxHeap
+//removing k-1 values from maxHeap
+// peeking the top value after removing k-1 values
