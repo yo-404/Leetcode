6c1bbeef35492af081eaca7d3fb2114b3ce3758b
yo-404 <yogeshy2801@gmail.com>
2023-08-14 23:21:19 +0530

    optimal and naive approach

--- 215. Kth Largest Element in an Array.cpp
+++ 215. Kth Largest Element in an Array.cpp
@@ -21,3 +21,14 @@ class Solution {
 // pushing all the values in maxHeap
 //removing k-1 values from maxHeap
 // peeking the top value after removing k-1 values
+
+-------------------
+
+// naive approach
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+    }
+}
