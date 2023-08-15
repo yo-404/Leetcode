63cc845b56c41c30c57387374b04a161bb6d4e25
yo-404 <yogeshy2801@gmail.com>
2023-08-15 23:12:05 +0530

    XOR array

---
+++ 2433. Find The Original Array of Prefix Xor.cpp
@@ -0,0 +1,14 @@
+//  creating a additional array to store the XOR'd array 
+
+class Solution {
+    public int[] findArray(int[] pref) {
+        int n=pref.length;
+        int[] arr = new int [n];
+        arr[0]=pref[0];
+
+        for(int i=1;i<n;i++){
+            arr[i]=pref[i-1]^pref[i];
+        }
+        return arr;
+    }
+}
